4ee1f1c789ea98d3d3f73e1ada4cc0d05971f0ff
beccabica <rebeca.3955@aluno.pr.senac.br>
2026-02-23 16:32:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if(idadeUser  >=18 && estadoUser === "PR") {
     document.getElementById("meuDinDin").innerText = "Hora de dar tchauuuuu!!!";
     document.getElementById("vish").innerText = "Agora só paciencia que o tempo te dá idade ";
     document.getElementById("ceTaDoido").innerText = "Vá ajudar a pagar as contas que seus pais pagam querida,vai lá!";
-    document.getElementById("dominique").src = "https://external-preview.redd.it/hvvjislTBrCdjJzZFBWjB-YJULnd_ctB2tYINq2RzNs.jpg?auto=webp&s=e7ca588ff24cfee4429f30d07c88be9a927ee663";
+    document.getElementById("dominique").src = "https://chc.org.br/wp-content/uploads/2012/12/flickr-por-alvin-trusty-600x400.jpg";
 
 
 } else {
@@ -15,3 +15,4 @@ if(idadeUser  >=18 && estadoUser === "PR") {
     document.getElementById("ceTaDoido").innerText = "Usa essa mente aí pra estudar! pra ser alguém na vida"  ; 
     document.getElementById("dominique").src = "https://chc.org.br/wp-content/uploads/2012/12/flickr-por-alvin-trusty-600x400.jpg";
 }
+
